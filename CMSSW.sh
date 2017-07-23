230f27525ef262da7c4fb19f8ac5953d76734d11
Shervin <shervin@cern.ch>
2017-07-23 11:52:29 +0200

    improved handling of files not in the same T2 where the jobs are running

--- CMSSW.sh
+++ CMSSW.sh
@@ -5,16 +5,28 @@ filelist=$2
 CMSSW_VERSION=$3
 
 tar -xzf sandbox.tgz 
-files=`grep "^$jobid " $filelist | cut -d ' ' -f 2 | tr '\n' ','`
-echo $files
-echo "process.source.fileNames = cms.untracked.vstring( [ $files ])" >> pset.py
-tail pset.py
-echo "========================================"
 
 cd $CMSSW_VERSION/
 scram build projectrename
 eval `scramv1 runtime -sh`
 cd -
+
+files=`grep "^$jobid " $filelist | cut -d ' ' -f 2 | tr '\n' ','`
+echo $files
+root -l -b -q `echo $files |tr ',' ' ' | tr '"' ' ' ` 2>&1 | tee xrootd_test.log
+grep -q Warning xrootd_test.log && {
+	for file in `echo $files | tr ',' ' ' | tr '"' ' '`
+	do
+		xrdcp -vsN $file . || exit 10
+	done
+	files=`echo $files | sed 's|".*/|"file:|'`
+}
+echo $files
+echo "process.source.fileNames = cms.untracked.vstring( [ $files ])" >> pset.py
+echo "------------------------------"
+tail pset.py
+echo "========================================"
+
 echo $jobid $filelist pset.py $CMSSW_BASE
 echo "======================================== ls"
 ls
@@ -26,6 +38,11 @@ cmsRun pset.py ||  {
 }
 echo "======================================== ls"
 ls
+for file in `echo $files | tr '"' ' ' | tr ',' ' ' | sed 's|file:||g`
+do
+	rm $file
+done
+
 echo "======================================== Copy"
 ./copy config $jobid || exit $((60000+$?))
 echo "======================================== END"
